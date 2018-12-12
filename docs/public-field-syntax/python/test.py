497617fb3ff21aa6cc2d53598e9bf15180f07af5
HE Shi-Jun <hax@heshijun.net>
2018-12-13 05:53:53 +0800

    add dataclass in python sample of public field syntax

--- docs/public-field-syntax/python/test.py
+++ docs/public-field-syntax/python/test.py
@@ -1,10 +1,22 @@
 a = 1
-b = a
+b = a * a
 
-print a, b
+print(a, b)
+
 
 class ClassVar:
 	a = 2
-	b = a
+	b = a * a
 
-print ClassVar.a, ClassVar.b
+print(ClassVar.a, ClassVar.b)
+
+
+from dataclasses import dataclass
+
+@dataclass
+class Instance:
+	a = 3
+	b = a * a
+
+inst = Instance()
+print(inst.a, inst.b)
