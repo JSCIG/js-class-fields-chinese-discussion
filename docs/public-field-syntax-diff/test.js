17ba15becd84e1245bdc4f98f42b53842efd3f2f
HE Shi-Jun <hax@heshijun.net>
2019-01-06 01:47:24 +0800

    confusion

--- docs/public-field-syntax-diff/test.js
+++ docs/public-field-syntax-diff/test.js
@@ -3,7 +3,7 @@ function test() {
 	a = 1
 	b = 2
 	c = 3
-	d = 4
+	d = b ** 2
 	e = 5
 	f = 6
 	g = 7
@@ -12,7 +12,7 @@ function test() {
 		a = 1
 		b = 2
 		c = 3
-		d = 4
+		d = b ** 2
 		e = 5
 		f = 6
 		g = 7
@@ -22,7 +22,7 @@ function test() {
 			a = 1
 			b = 2
 			c = 3
-			d = 4
+			d = b ** 2
 			e = 5
 			f = 6
 			g = 7
@@ -31,7 +31,7 @@ function test() {
 				a = 1
 				b = 2
 				c = 3
-				d = 4
+				d = b ** 2
 				e = 5
 				f = 6
 				g = 7
