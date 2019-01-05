475bedb629325173f6d3c767433f1e4f99d158b4
HE Shi-Jun <hax@heshijun.net>
2019-01-06 01:41:41 +0800

    Update test.js

--- docs/public-field-syntax-diff/test.js
+++ docs/public-field-syntax-diff/test.js
@@ -1,5 +1,14 @@
 function test() {
-	class Test {
+
+	a = 1
+	b = 2
+	c = 3
+	d = 4
+	e = 5
+	f = 6
+	g = 7
+
+	return class Test {
 		a = 1
 		b = 2
 		c = 3
@@ -9,6 +18,7 @@ function test() {
 		g = 7
 
 		test() {
+
 			a = 1
 			b = 2
 			c = 3
